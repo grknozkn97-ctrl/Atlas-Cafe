8c52f6721621c653330dd04602a17bd8675ab6e1
grknozkn97-ctrl <grknozkn97@gmail.com>
2026-05-09 20:19:10 +0200

    Update Header.jsx

--- apps/web/src/components/Header.jsx
+++ apps/web/src/components/Header.jsx
@@ -43,7 +43,7 @@ const Header = () => {
         {/* Sol: Logo */}
         <a href="/" style={{ display: 'flex', alignItems: 'center', textDecoration: 'none', flexShrink: 0 }}>
           <img
-            src="/Atlas_Trspr.png"
+            src="/Atlas Trspr.png"
             alt="Atlas Cafe"
             style={{ height: '48px', width: 'auto', objectFit: 'contain' }}
           />
